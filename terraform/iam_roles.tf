5af59dbb36e345c1f26e865e5262124bebe098c3
Deiby Sierra <76053259+DeibyDev@users.noreply.github.com>
2024-09-27 23:36:10 +0000

    Se agrega el rol de ejecucion

---
+++ terraform/iam_roles.tf
@@ -0,0 +1,22 @@
+# Crear el rol de ejecución de ECS
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Effect    = "Allow"
+      Sid       = ""
+    }]
+  })
+}
+
+# Adjuntar la política de ejecución de tareas de ECS
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
